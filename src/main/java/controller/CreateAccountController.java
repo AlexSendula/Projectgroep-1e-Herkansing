b6701a0eb0edd80775a49b20cdc57e81ec8cf683
Alex Rojas Lopez <g.alexrl03@gmail.com>
2021-05-10 23:52:29 +0200

    Aangepaste versie (1.1) CreateAccountScherm

--- src/main/java/controller/CreateAccountController.java
+++ src/main/java/controller/CreateAccountController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class CreateAccountController {
+
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField lastNameTextField;
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private PasswordField confirmPasswordField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private Label incorrectLabel;
+
+
+    public void createAccount(MouseEvent mouseEvent) throws IOException {
+        if (userNameTextField.getText().equals("test") && passwordField.getText().equals("123") && confirmPasswordField.getText().equals("123")) {
+//              TODO: Exceptions maken voor alle ingevulde gegevens. Alle gegevens in bestand opslaan en gebruik maken van een User class. (?)
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
+            rootPane.getChildren().setAll(pane);
+        } else {
+            incorrectLabel.setVisible(true);
+        }
+    }
 }
