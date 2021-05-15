7bcfbaf497259beabf7ca91108df1f8cf44c45fb
meltvirus <74384292+meltvirus@users.noreply.github.com>
2021-05-15 18:48:35 +0200

    Login en user aangemaakt, login werkt nu met behulp van de classes

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import classes.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -12,7 +14,13 @@ public class HomeController {
     @FXML
     private AnchorPane rootPane;
 
-    public void back(MouseEvent mouseEvent) throws IOException {
+
+    public void shop(MouseEvent mouseEvent) throws IOException {
+
+    }
+
+    public void logOutEvent(MouseEvent mouseEvent) throws  IOException {
+        User.logOut();
         AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
         rootPane.getChildren().setAll(pane);
     }
