23ef24c4f650af4db2cf5dfdbccc9ad33f096702
Alex <a.sendula@gmail.com>
2021-05-30 16:38:33 +0200

    Product constructor

--- src/main/java/classes/Product.java
+++ src/main/java/classes/Product.java
@@ -1,4 +1,20 @@
 package classes;
 
+
+import java.util.ArrayList;
+
 public class Product {
+    private String name;
+    private String description;
+    private double price;
+    private ArrayList<String> categories;
+
+    public Product(String name, String description, double price, ArrayList<String> categories) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.categories = categories;
+    }
 }
+
+
