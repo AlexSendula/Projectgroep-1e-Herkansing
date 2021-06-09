0d4645d9f016de1f028aac14933ff60198767f5e
meltvirus <74384292+meltvirus@users.noreply.github.com>
2021-06-09 23:10:37 +0200

    CreateAccountTest

---
+++ src/test/java/Account/CreateAccountTest.java
@@ -0,0 +1,20 @@
+package Account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateAccountTest {
+
+    @Test
+     void createAccountPossible() {
+        String[] userInfo = {"Kees", "Jan", "2021-06-09", "Jan", "wachtwoord", "wachtwoord", "hendrik@asd.nl"};
+        String[] userInfoCorr = {"Kees","Jan","2021-06-09","Jan123","wachtwoord","wachtwoord","hendrik123@asd.nl"};
+
+        String[] wachtwoordCheck = {"Kees","Jan","2021-06-09","Jan123","wachtwoor","wachtwoord","hendrik123@asd.nl"};
+
+        assertTrue(new CreateAccount().createAccountPossible(userInfoCorr));
+        assertFalse(new CreateAccount().createAccountPossible(userInfo));
+        assertFalse(new CreateAccount().createAccountPossible(wachtwoordCheck));
+    }
+}
