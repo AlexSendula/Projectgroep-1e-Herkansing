03b98c13df43e531f032160a6f5817e06f77c35c
ArmanFara <armanjohn@live.nl>
2021-06-10 21:04:32 +0200

    ShoppingCart klasse

--- src/main/java/Account/User.java
+++ src/main/java/Account/User.java
@@ -1,10 +1,14 @@
 package Account;
 
+import Shop.ShoppingCart;
+
 public class User {
     private String username;
     private Login login;
+    private ShoppingCart shoppingCart;
 
     public User(String username, String password){
+        this.shoppingCart = new ShoppingCart();
         this.username = username;
         this.login = Login.getInstance(this.username, password);
     }
