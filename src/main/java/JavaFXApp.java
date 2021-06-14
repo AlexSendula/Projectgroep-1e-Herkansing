10a38b9652578cbcf6573ec8d3d33f3ae317d2e6
Alex <a.sendula@gmail.com>
2021-06-14 15:19:18 +0200

    shoppingcart start

--- src/main/java/JavaFXApp.java
+++ src/main/java/JavaFXApp.java
@@ -12,7 +12,7 @@ public class JavaFXApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/ShopScreen.fxml"));
 
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("ZeroXess");
