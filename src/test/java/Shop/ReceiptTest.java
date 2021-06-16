a3bcc04f7e749e24948402709af5b10871b61026
ArmanFara <armanjohn@live.nl>
2021-06-16 21:07:27 +0200

    ReceiptTest vernieuwd

--- src/test/java/Shop/ReceiptTest.java
+++ src/test/java/Shop/ReceiptTest.java
@@ -16,21 +16,22 @@ class ReceiptTest {
     @Test
     void testToString() throws IOException {
         ShopController shop = new ShopController();
-        User user = new User("Jan","Hendrik");
+        String[] userInfo = {"","","","Jan","123","@"};
+        User user = new User(userInfo);
         shop.parseData();
         ArrayList<Product> products = new ArrayList<>(shop.getProductList());
         user.getShoppingCart().addToCart(products.get(0));
         user.getShoppingCart().addToCart(products.get(1));
         Receipt receipt = new Receipt(user);
-        assertTrue(receipt.toString().equals("Customer Jan has purchased the following products:\n" +
+        assertEquals("Customer Jan has purchased the following products:\n" +
                 "Laptop, Televisie.\n" +
                 "The total price: 644.98.\n" +
                 "Your discount is: 0.0%.\n" +
-                "Your price after discount: 644.98"));
-        assertFalse(receipt.toString().equals("Customer Hendrik has purchased the following products:\n" +
+                "Your price after discount: 644.98", receipt.toString());
+        assertNotEquals("Customer Hendrik has purchased the following products:\n" +
                 "Laptop, Televisie.\n" +
                 "The total price: 644.98.\n" +
                 "Your discount is: 0.0%.\n" +
-                "Your price after discount: 644.98"));
+                "Your price after discount: 644.98", receipt.toString());
     }
 }
