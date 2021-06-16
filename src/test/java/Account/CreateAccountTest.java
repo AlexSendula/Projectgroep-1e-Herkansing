a53094ed6cc5dfb130bb17028619dade055b47b9
ArmanFara <armanjohn@live.nl>
2021-06-16 19:39:17 +0200

    CreateAccountTest vernieuwd

--- src/test/java/Account/CreateAccountTest.java
+++ src/test/java/Account/CreateAccountTest.java
@@ -2,20 +2,36 @@ package Account;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateAccountTest {
 
     //Coverage: decision
     @Test
-     void createAccountPossible() {
-        String[] userInfo = {"Kees", "Jan", "2021-06-09", "Jan", "wachtwoord", "wachtwoord", "hendrik@asd.nl"};
-        String[] userInfoCorr = {"Kees","Jan","2021-06-09","Jan123","wachtwoord","wachtwoord","hendrik123@asd.nl"};
+     void createAccountPossible() throws IOException {
+        CreateAccountController controller = new CreateAccountController();
+        controller.parseData();
+        ArrayList<String> userInfo = new ArrayList<>();
+        userInfo.add("Kees");
+        userInfo.add("Jan");
+        userInfo.add("2021-06-09");
+        userInfo.add("Jan");
+        userInfo.add("wachtwoord");
+        userInfo.add("wachtwoor");
+        userInfo.add("hendrik@asd.nl");
+        assertFalse(new CreateAccount().createAccountPossible(userInfo,controller.getUserList()));
+        userInfo.remove(5);
+        userInfo.add(5,"wachtwoord");
+        assertTrue(new CreateAccount().createAccountPossible(userInfo,controller.getUserList()));
 
-        String[] wachtwoordCheck = {"Kees","Jan","2021-06-09","Jan123","wachtwoor","wachtwoord","hendrik123@asd.nl"};
-
-        //assertTrue(new CreateAccount().createAccountPossible(userInfoCorr));
-        //assertFalse(new CreateAccount().createAccountPossible(userInfo));
-        //assertFalse(new CreateAccount().createAccountPossible(wachtwoordCheck));
+        userInfo.remove(3);
+        userInfo.add(3,"dylan");
+        assertFalse(new CreateAccount().createAccountPossible(userInfo,controller.getUserList()));
+        userInfo.remove(3);
+        userInfo.add(3,"nietdylan");
+        assertTrue(new CreateAccount().createAccountPossible(userInfo,controller.getUserList()));
     }
 }
